71acbdf48c6b902649e51fdae5b3ef52088005b2
AnnAnnFryingPan <ann.gledson@manchester.ac.uk>
2019-11-07 14:28:22 +0000

    Updating settings for dev and deployment

--- geo_sensor_gaps/settings/base.py
+++ geo_sensor_gaps/settings/base.py
@@ -9,18 +9,19 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-
+from __future__ import absolute_import, unicode_literals
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+#PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(PROJECT_DIR)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5e#ol-mirtx-xjoe=#@wk8tvuv=yuoq(2ywd^)y8=8o2c+6v#3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,19 +75,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'geo_sensor_gaps.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'sensor_gaps',
-        'USER': 'mcassag',
-        'PASSWORD': 'sensor_gaps_pw',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
 
 
 # Password validation
@@ -129,6 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- geo_sensor_gaps/wsgi.py
+++ geo_sensor_gaps/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
+from __future__ import absolute_import, unicode_literals
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geo_sensor_gaps.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geo_sensor_gaps.settings.dev")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geo_sensor_gaps.settings.production")
 
 application = get_wsgi_application()
