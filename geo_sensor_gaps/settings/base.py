238d69a01820db1e5c780d9dee0237aab9f0043f
AnnAnnFryingPan <ann.gledson@manchester.ac.uk>
2020-04-28 20:10:47 +0100

    settings updates

--- geo_sensor_gaps/settings/base.py
+++ geo_sensor_gaps/settings/base.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 from __future__ import absolute_import, unicode_literals
 import os
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
