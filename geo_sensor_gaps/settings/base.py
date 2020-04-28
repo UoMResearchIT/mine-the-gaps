378faefa4c527fe6b0fd2b46373eca621533750b
AnnAnnFryingPan <ann.gledson@manchester.ac.uk>
2020-04-28 20:30:09 +0100

    STATIC_URL and MEDIA_URL now declared in local.py, removed from base.py

--- geo_sensor_gaps/settings/base.py
+++ geo_sensor_gaps/settings/base.py
@@ -123,7 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
