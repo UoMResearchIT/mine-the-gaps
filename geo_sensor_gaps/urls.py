b62152e5242bbf45d383c983085f302b81aff5a3
annannfryingpan <ann.gledson@manchester.ac.uk>
2021-03-15 17:38:15 +0000

    Added registration

--- geo_sensor_gaps/urls.py
+++ geo_sensor_gaps/urls.py
@@ -89,3 +89,7 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+
